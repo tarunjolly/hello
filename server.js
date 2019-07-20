7f5cce85a4f8434d156942bc50127402b25e8bcb
Tarun Jolly <tarunjolly8@gmail.com>
2019-07-20 20:05:31 +0530

    hey
    Signed-off-by: Tarun Jolly <tarunjolly8@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express=req('express')
+const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
     res.send('hello')
 })
 const port=process.env.port||4444
-app.listen('port')
+app.listen(port)
